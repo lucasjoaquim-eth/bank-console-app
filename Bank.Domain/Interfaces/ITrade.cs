a07538606fe579b88a34267b257f3986dd170688
Lucas Joaquim <31598407+lucasjoaquim-eth@users.noreply.github.com>
2022-10-24 01:31:09 -0300

    First commit

---
+++ Bank.Domain/Interfaces/ITrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Domain.Interfaces
+{
+    public interface ITrade
+    {
+        DateTime NextPaymentDate { get;  }
+        double Value { get;  }
+        string ClientSector { get;  }
+        List<ITrade> Trades { get;  }
+    }
+}
